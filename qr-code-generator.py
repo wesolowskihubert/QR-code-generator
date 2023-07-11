021649d49d2334047f66a650be8befb49dda4cd2
wesolowskihubert <hwesolowski@edu.cdv.pl>
2023-07-11 17:24:44 +0200

    qr code generator

---
+++ qr-code-generator.py
@@ -0,0 +1,18 @@
+import qrcode
+
+input_URL = "https://github.com/wesolowskihubert"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=15,
+    border=4,
+)
+
+qr.add_data(input_URL)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qrcode.png")
+
+print(qr.data_list)
